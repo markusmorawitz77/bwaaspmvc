5d908ea60de9a540c02d0630ed1f18a35176fba3
Markus Morawitz <markus.morawitz@gmx.com>
2021-02-02 07:32:49 +0100

    Add first controller

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository PieRepository;
+        private readonly ICategoryRepository CategoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            PieRepository = pieRepository;
+            CategoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(PieRepository.AllPies);
+        }
+    }
+}
